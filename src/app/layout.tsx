78176452513fdb775b6f44387aef92ed4907fefd
Abhijit47 <akarmakar846@gmail.com>
2026-02-21 13:55:13 +0530

    fix the svg for both mode(dark and light)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,90 +20,108 @@ const ibm_plex_mono = IBM_Plex_Mono({
   variable: '--font-mono',
 });
 
-export const metadata: Metadata = {
-  title: 'PureLanding - Beautiful Shadcn UI Landing Page',
-  description:
-    'A beautiful landing page built with Shadcn UI, Next.js 15, Tailwind CSS, and Shadcn UI Blocks.',
-  keywords: [
-    'PureLanding',
-    'PureLanding Landing Page',
-    'PureLanding Landing Page Template',
-    'PureLanding Page',
-    'Shadcn UI Landing Page',
-    'Shadcn UI Blocks',
-    'Shadcn UI',
-    'Landing Page',
-    'Tailwind CSS Landing Page',
-    'Beautiful Shadcn UI Landing Page',
-    'Next.js 15 Landing Page',
-    'Simple Landing Page',
-    'Landing Page Template',
-    'Landing Page Design',
-  ],
-  openGraph: {
-    type: 'website',
-    siteName: 'PureLanding',
-    locale: 'en_US',
-    url: 'https://shadcn-landing-page.vercel.app',
-    title: 'PureLanding - Beautiful Shadcn UI Landing Page',
-    description:
-      'A beautiful landing page built with Shadcn UI, Next.js 15, Tailwind CSS, and Shadcn UI Blocks.',
-    images: [
-      {
-        url: '/favicons/og-image.jpg',
-        width: 1200,
-        height: 630,
-        alt: 'PureLanding Preview',
-      },
-    ],
-  },
-  authors: [
-    {
-      name: 'Akash Moradiya',
-      url: 'https://shadcnui-blocks.com',
+// dynamic metadata
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    icons: {
+      icon: '/favicons/favicon.svg',
     },
-  ],
-  creator: 'Akash Moradiya',
-  icons: [
-    {
-      rel: 'icon',
-      url: '/favicons/favicon.ico',
-    },
-    {
-      rel: 'apple-touch-icon',
-      url: '/favicons/favicon.svg',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      url: '/favicons/favicon.svg',
-      sizes: '32x32',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      url: '/favicons/favicon.svg',
-      sizes: '16x16',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      url: '/favicons/favicon.svg',
-      sizes: '192x192',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      url: '/favicons/favicon.svg',
-      sizes: '512x512',
-    },
-  ],
-  robots: {
-    index: true,
-    follow: true,
-  },
-  manifest: '/favicons/site.webmanifest',
-};
+  };
+}
+
+// static metadata
+// export const metadata: Metadata = {
+//   icons: {
+//     icon: '/favicons/favicon.svg', // Points to public/favicons/favicon.svg
+//     // You can also add more specific types:
+//     // apple: '/favicons/apple-touch-icon.png',
+//   },
+// };
+
+// export const metadata: Metadata = {
+//   title: 'PureLanding - Beautiful Shadcn UI Landing Page',
+//   description:
+//     'A beautiful landing page built with Shadcn UI, Next.js 15, Tailwind CSS, and Shadcn UI Blocks.',
+//   keywords: [
+//     'PureLanding',
+//     'PureLanding Landing Page',
+//     'PureLanding Landing Page Template',
+//     'PureLanding Page',
+//     'Shadcn UI Landing Page',
+//     'Shadcn UI Blocks',
+//     'Shadcn UI',
+//     'Landing Page',
+//     'Tailwind CSS Landing Page',
+//     'Beautiful Shadcn UI Landing Page',
+//     'Next.js 15 Landing Page',
+//     'Simple Landing Page',
+//     'Landing Page Template',
+//     'Landing Page Design',
+//   ],
+//   openGraph: {
+//     type: 'website',
+//     siteName: 'PureLanding',
+//     locale: 'en_US',
+//     url: 'https://shadcn-landing-page.vercel.app',
+//     title: 'PureLanding - Beautiful Shadcn UI Landing Page',
+//     description:
+//       'A beautiful landing page built with Shadcn UI, Next.js 15, Tailwind CSS, and Shadcn UI Blocks.',
+//     images: [
+//       {
+//         url: '/favicons/og-image.jpg',
+//         width: 1200,
+//         height: 630,
+//         alt: 'PureLanding Preview',
+//       },
+//     ],
+//   },
+//   authors: [
+//     {
+//       name: 'Akash Moradiya',
+//       url: 'https://shadcnui-blocks.com',
+//     },
+//   ],
+//   creator: 'Akash Moradiya',
+//   icons: [
+//     {
+//       rel: 'icon',
+//       url: '/favicons/favicon.ico',
+//     },
+//     {
+//       rel: 'apple-touch-icon',
+//       url: '/favicons/favicon.svg',
+//     },
+//     {
+//       rel: 'icon',
+//       type: 'image/png',
+//       url: '/favicons/favicon.svg',
+//       sizes: '32x32',
+//     },
+//     {
+//       rel: 'icon',
+//       type: 'image/png',
+//       url: '/favicons/favicon.svg',
+//       sizes: '16x16',
+//     },
+//     {
+//       rel: 'icon',
+//       type: 'image/png',
+//       url: '/favicons/favicon.svg',
+//       sizes: '192x192',
+//     },
+//     {
+//       rel: 'icon',
+//       type: 'image/png',
+//       url: '/favicons/favicon.svg',
+//       sizes: '512x512',
+//     },
+//   ],
+//   robots: {
+//     index: true,
+//     follow: true,
+//   },
+//   manifest: '/favicons/site.webmanifest',
+// };
 
 export default function AppLayout({
   children,
@@ -115,6 +133,7 @@ export default function AppLayout({
       lang='en'
       className={`${lato.variable} ${nunito.variable} ${ibm_plex_mono.variable}`}
       suppressHydrationWarning>
+      <link rel='icon' href='/favicons/favicon.svg' />
       <body className={`font-body antialiased`}>
         <Providers>{children}</Providers>
       </body>
